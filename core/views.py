1d69482ee6699677085617f25daa9767721545e7
Gapprin <mate.alvarez@duocuc.cl>
2024-10-17 00:16:01 -0300

    Update Permisos Page_404

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import logout_then_login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import *
 from .forms import *
 
 # Create your views here.
@@ -36,9 +37,17 @@ def registrarse(request):
         registro = Registro()
     return render(request, "registrarse.html", {"form" : registro})
 
+# lambda sirve para crear mini funciones, para evitar usar el def
+@user_passes_test(lambda user: user.is_superuser)
 def pagAdmin(request):
     return render(request, 'pagAdmin.html')
 
+@login_required
 def historial(request):
     registros = SolicitudRetiro.objects.filter(usuario = request.user.id)
     return render(request, 'historial.html', {"registros" : registros})
+
+def error_404(request, exception):
+    return render(request, 'Error/error_404.html', status=404)
+
+handler404 = error_404
